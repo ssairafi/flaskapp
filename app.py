f10c4ffd948ebcab7dc95a0e2cf38aac4fc990a9
Salman <sairafi@gmail.com>
2025-02-11 12:36:38 +0300

    added route

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return 'Hello, World!'
+@app.route('/hello')
+def hello():
+    return app.send_static_file('hello.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
